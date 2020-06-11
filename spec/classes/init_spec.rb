e6c23edec2cc8196617758bdc5b8ca1043cd307a
David Llorente <d.llorente@billymob.com>
2020-06-11 11:50:34 +0200

    Added test for graphite emitter

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -321,4 +321,31 @@ describe 'druid', :type => 'class' do
     }
   end
 
+  context 'On base system with graphite as emitter' do
+    let(:facts) do
+      {
+        :memorysize      => '10 GB',
+        :ipaddress       => '127.0.0.1',
+        :osfamily        => 'Darwin',
+        :operatingsystem => 'Darwin',
+        :architecture    => 'x86_64',
+      }
+    end
+
+    let(:params) do
+      {
+        :emitter                                      => 'graphite',
+        :emitter_graphite_hostname                    => 'graphitehost.com',
+        :emitter_graphite_port                        => 2004,
+        :emitter_graphite_batchSize                   => 200,
+        :emitter_graphite_eventConverter              => { 'type' => 'whiteList', 'namespacePrefix' => 'someprefix', 'ignoreHostname' => false, 'ignoreServiceName' => false, 'mapPath' => '/somefile.json' },
+        :emitter_graphite_flushPeriod                 => 120000,
+      }
+    end
+
+    it {
+      should contain_file('/etc/druid/common.runtime.properties').with_content("# This file is managed by Puppet\n# MODIFICATION WILL BE OVERWRITTEN\n\n# Extensions\ndruid.extensions.remoteRepositories=[\"http://repo1.maven.org/maven2/\", \"https://metamx.artifactoryonline.com/metamx/pub-libs-releases-local\"]\ndruid.extensions.localRepository=~/.m2/repository\ndruid.extensions.coordinates=[]\ndruid.extensions.searchCurrentClassloader=true\ndruid.extensions.hadoopDependenciesDir=/opt/druid/hadoop-dependencies\n\n# Zookeeper\ndruid.zk.paths.base=/druid\ndruid.zk.service.host=localhost\ndruid.zk.service.sessionTimeoutMs=30000\ndruid.curator.compress=true\ndruid.discovery.curator.path=/druid/discovery\n\n# Request Logging\ndruid.request.logging.type=noop\n\n# Enabling Metrics\ndruid.monitoring.emissionPeriod=PT1m\n\n# Emitting Metrics\ndruid.emitter=graphite\ndruid.emitter.graphite.hostname=graphitehost.com\ndruid.emitter.graphite.port=2004\ndruid.emitter.graphite.batchSize=200\ndruid.emitter.graphite.eventConverter={\"type\":\"whiteList\",\"namespacePrefix\":\"someprefix\",\"ignoreHostname\":false,\"ignoreServiceName\":false,\"mapPath\":\"/somefile.json\"}\ndruid.emitter.graphite.flushPeriod=120000\n\n# Metadata Storage\ndruid.metadata.storage.type=mysql\ndruid.metadata.storage.connector.connectURI=jdbc:mysql://localhost:3306/druid?characterEncoding=UTF-8\ndruid.metadata.storage.connector.user=druid\ndruid.metadata.storage.connector.password=insecure_pass\ndruid.metadata.storage.connector.createTables=true\ndruid.metadata.storage.tables.base=druid\ndruid.metadata.storage.tables.segmentTable=druid_segments\ndruid.metadata.storage.tables.ruleTable=druid_rules\ndruid.metadata.storage.tables.configTable=druid_config\ndruid.metadata.storage.tables.tasks=druid_tasks\ndruid.metadata.storage.tables.taskLog=druid_taskLog\ndruid.metadata.storage.tables.taskLock=druid_taskLock\ndruid.metadata.storage.tables.audit=druid_audit\n\n# Deep Storage\ndruid.storage.type=local\ndruid.storage.storageDirectory=/tmp/druid/localStorage\n\n# Caching\ndruid.cache.type=local\ndruid.cache.sizeInBytes=0\ndruid.cache.initialSize=500000\ndruid.cache.logEvictionCount=0\n\n# Indexing Service Discovery\ndruid.selectors.indexing.serviceName=druid/overlord\n\n# Coordinator Service Discovery\ndruid.selectors.coordinator.serviceName=druid/coordinator\n\n# Logging\n# #\n#\n# # Log all runtime properties on startup. Disable to avoid logging properties on startup:\n\ndruid.startup.logging.logProperties=true\n#\n# Announcing Segments\ndruid.announcer.type=batch\ndruid.announcer.segmentsPerNode=50\ndruid.announcer.maxBytesPerNode=524288\n# Task Logging\ndruid.indexer.logs.type=file\ndruid.indexer.logs.directory=/var/log\n")
+    }
+  end
+
 end
